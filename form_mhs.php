ad07c4fa7b233cf58504b9ecb83d01232399d503
narulitaap <narulita.arien@gmail.com>
2022-04-02 21:10:09 +0700

    Menambahkan file form_mhs.php

---
+++ form_mhs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Tambah Mahasiswa</title>
+</head>
+<body>
+    <h1>Form Tambah Mahasiswa</h1>
+    <form method="POST" action="action.php">
+        <p>Nama Mahasiswa : <input type="text" name="nama_mhs"></p><br/>
+        <p>NIM Mahasiswa : <input type="text" name="nim_mhs"></p><br/>
+        <p>Alamat Mahasiswa : <input type="text" name="alamat_mhs"></p><br/>
+        <button type="submit">Kirim</button>
+    </form>
+    
+</body>
+</html>
