57d04302285b949859368a2c1ec69eb19fc2997f
narulitaap <narulita.arien@gmail.com>
2022-04-02 21:06:39 +0700

    Menambahkan file koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "" ;
+$dbname = "arin";
+
+$koneksi = mysqli_connect($host,$user,$password,$dbname);
+//if(!$koneksi){
+//    echo "koneksi gagal";
+//}else{
+//   echo "koneksi berhasil";
+//}
+?>
